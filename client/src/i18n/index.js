c12194530f7c854b193ba5d33603e007b7c07707
Harry Chen <harrychen0314@gmail.com>
2021-07-15 10:15:43 -0700

    import frontend from https://github.com/chenhongqiao/TOJ-Frontend

---
+++ client/src/i18n/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import VueI18n from 'vue-i18n'
+// ivew UI
+import ivenUS from 'iview/dist/locale/en-US'
+// element UI
+import elenUS from 'element-ui/lib/locale/lang/en'
+
+Vue.use(VueI18n)
+
+const languages = [
+  {value: 'en-US', label: 'English', iv: ivenUS, el: elenUS}
+]
+const messages = {}
+
+// combine admin and oj
+for (let lang of languages) {
+  let locale = lang.value
+  let m = require(`./oj/${locale}`).m
+  Object.assign(m, require(`./admin/${locale}`).m)
+  let ui = Object.assign(lang.iv, lang.el)
+  messages[locale] = Object.assign({m: m}, ui)
+}
+// load language packages
+export default new VueI18n({
+  locale: 'en-US',
+  messages: messages
+})
+
+export {languages}
---
+++ client/src/pages/admin/components/SideMenu.vue
@@ -0,0 +1,65 @@
+<template>
+  <el-menu class="vertical_menu"
+           :router="true" :default-active="currentPath">
+    <div class="logo">
+      <img src="../../../assets/logo.svg" alt="oj admin"/>
+    </div>
+    <el-menu-item index="/"><i class="el-icon-fa-dashboard"></i>{{$t('m.Dashboard')}}</el-menu-item>
+    <el-submenu v-if="isSuperAdmin" index="general">
+      <template slot="title"><i class="el-icon-menu"></i>{{$t('m.General')}}</template>
+      <el-menu-item index="/user">{{$t('m.User')}}</el-menu-item>
+      <el-menu-item index="/announcement">{{$t('m.Announcement')}}</el-menu-item>
+      <el-menu-item index="/conf">{{$t('m.System_Config')}}</el-menu-item>
+      <el-menu-item index="/judge-server">{{$t('m.Judge_Server')}}</el-menu-item>
+      <el-menu-item index="/prune-test-case">{{$t('m.Prune_Test_Case')}}</el-menu-item>
+    </el-submenu>
+    <el-submenu index="contest">
+      <template slot="title"><i class="el-icon-fa-trophy"></i>{{$t('m.Contest')}}</template>
+      <el-menu-item index="/contest">{{$t('m.Contest_List')}}</el-menu-item>
+      <el-menu-item index="/contest/create">{{$t('m.Create_Contest')}}</el-menu-item>
+    </el-submenu>
+  </el-menu>
+</template>
+
+<script>
+  import {mapGetters} from 'vuex'
+
+  export default {
+    name: 'SideMenu',
+    data () {
+      return {
+        currentPath: ''
+      }
+    },
+    mounted () {
+      this.currentPath = this.$route.path
+    },
+    computed: {
+      ...mapGetters(['user', 'isSuperAdmin', 'hasProblemPermission'])
+    }
+  }
+</script>
+
+<style scoped lang="less">
+  .vertical_menu {
+    overflow: auto;
+    width: 205px;
+    height: 100%;
+    position: fixed !important;
+    z-index: 100;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    .logo {
+      margin: 20px 0;
+      text-align: center;
+      img {
+        background-color: #fff;
+        border-radius: 50%;
+        border: 3px solid #fff;
+        width: 75px;
+        height: 75px;
+      }
+    }
+  }
+</style>
