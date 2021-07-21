ce0c3f0cd02cc3ced39bb127fb103d76af989d04
Harry Chen <harrychen0314@gmail.com>
2021-07-20 23:00:01 -0700

    fix editor bugs

--- client/src/pages/admin/components/Simditor.vue
+++ client/src/pages/admin/components/Simditor.vue
@@ -3,18 +3,15 @@
 </template>
 
 <script>
-  import Simditor from 'tar-simditor'
-  import 'tar-simditor/styles/simditor.css'
-  import 'tar-simditor-markdown'
-  import 'tar-simditor-markdown/styles/simditor-markdown.css'
-  import './simditor-file-upload'
+  import Simditor from 'simditor'
+  import 'simditor/styles/simditor.css'
 
   export default {
     name: 'Simditor',
     props: {
       toolbar: {
         type: Array,
-        default: () => ['title', 'bold', 'italic', 'underline', 'fontScale', 'color', 'ol', 'ul', '|', 'link', 'image', 'uploadfile', 'hr', '|', 'indent', 'outdent', 'alignment', '|', 'markdown']
+        default: () => ['title', 'bold', 'italic', 'underline', 'fontScale', 'color', 'ol', 'ul', '|', 'link', 'image', 'hr', '|', 'indent', 'outdent', 'alignment']
       },
       value: {
         type: String,
@@ -35,13 +32,6 @@
         toolbar: this.toolbar,
         pasteImage: true,
         markdown: false,
-        upload: {
-          url: '/api/admin/upload_image/',
-          params: null,
-          fileKey: 'image',
-          connectionCount: 3,
-          leaveConfirm: this.$i18n.t('m.Uploading_is_in_progress')
-        },
         allowedStyles: {
           span: ['color']
         }
