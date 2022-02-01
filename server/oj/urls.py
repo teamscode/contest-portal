be58061339a4c76a8efcf6f0836f34882248afe0
Harry Chen <harrychen0314@gmail.com>
2022-01-31 21:46:33 -0800

    cleanup

--- server/oj/urls.py
+++ server/oj/urls.py
@@ -13,5 +13,4 @@ urlpatterns = [
     url(r"^api/admin/", include("contest.urls.admin")),
     url(r"^api/", include("submission.urls.oj")),
     url(r"^api/admin/", include("submission.urls.admin")),
-    url(r"^api/admin/", include("utils.urls")),
 ]
