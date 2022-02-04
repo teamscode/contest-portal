adc377cf0f8af240b86d6dd7e70dbf486b814151
Harry Chen <harrychen0314@gmail.com>
2022-02-04 10:29:59 -0800

    fix ranklist

--- server/utils/api/_serializers.py
+++ server/utils/api/_serializers.py
@@ -4,6 +4,7 @@ from rest_framework import serializers
 class UsernameSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     username = serializers.CharField()
+    team_name = serializers.SerializerMethodField()
     team_members = serializers.SerializerMethodField()
 
     def __init__(self, *args, **kwargs):
@@ -12,3 +13,6 @@ class UsernameSerializer(serializers.Serializer):
 
     def get_team_members(self, obj):
         return obj.userprofile.team_members if self.need_team_members else None
+
+    def get_team_name(self, obj):
+        return obj.userprofile.team_name
