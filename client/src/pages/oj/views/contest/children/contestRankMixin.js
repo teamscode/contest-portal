219ebc33212c2ac561474771471f2f056ec04810
Harry Chen <harrychen0314@gmail.com>
2022-04-05 23:29:13 -0700

    show username in rank

--- client/src/pages/oj/views/contest/children/contestRankMixin.js
+++ client/src/pages/oj/views/contest/children/contestRankMixin.js
@@ -74,7 +74,7 @@ export default {
             fixed: 'left',
             minWidth: 220,
             render: (h, {row}) => {
-              return h('span', row.user.team_members.map(member => member.name).join(', '))
+              return h('span', row.user.team_members.map(member => member.name).join(', ') + ' (' + row.user.username + ')')
             }
           })
         } else {
