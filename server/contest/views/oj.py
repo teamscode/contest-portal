5b56aecc0a06969748d2bd96e5e538ad49648612
Harry Chen <harrychen0314@gmail.com>
2022-08-06 13:43:16 -0700

    rebuild image
    Signed-off-by: Harry Chen <harrychen0314@gmail.com>

--- server/contest/views/oj.py
+++ server/contest/views/oj.py
@@ -166,6 +166,7 @@ class ContestRankAPI(APIView):
                     serialized_team_members += ";"
                     serialized_team_members += member["email"]
                     serialized_team_members += ";"
+                    # convert
                     serialized_team_members += str(member["year"])
                     serialized_team_members += ";"
                     serialized_team_members += member["parent_email"]
