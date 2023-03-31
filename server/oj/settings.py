c1f7997eedbce199d436a47755e65d6c50df13e5
Harry Chen <harrychen0314@gmail.com>
2023-03-30 21:05:41 -0700

    update allow headers
    Signed-off-by: Harry Chen <harrychen0314@gmail.com>

--- server/oj/settings.py
+++ server/oj/settings.py
@@ -252,4 +252,18 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
 
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+    'HTTP_APPKEY'
+]
+
 CORS_ALLOW_CREDENTIALS = True
